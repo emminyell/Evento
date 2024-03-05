e1351c679199123a69bf781d32bcc54f3b32c08d
emminyell <140811695+emminyell@users.noreply.github.com>
2024-03-05 23:27:33 +0000

    supression des événements

--- Evento/routes/web.php
+++ Evento/routes/web.php
@@ -15,13 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -33,6 +30,8 @@ Route::get('/auth', function () {
 
     // Route::resource('dashboard', EventController::class);
     Route::get('/dashboard', [EventController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+    Route::get('/', [EventController::class, 'welcome'])->name('welcome');
+    Route::delete('/events/{id}', [EventController::class, 'destroy'])->name('destroy');
 
 
 
