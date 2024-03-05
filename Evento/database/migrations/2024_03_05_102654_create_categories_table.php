6d7bb537d70f35aa1554ef9ab6f8c9f585862177
emminyell <140811695+emminyell@users.noreply.github.com>
2024-03-05 10:35:20 +0000

    ajouter la migration des tables

---
+++ Evento/database/migrations/2024_03_05_102654_create_categories_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->date('date');
+            $table->string('location');
+            $table->unsignedBigInteger('organizer_id');
+            $table->foreign('organizer_id')->references('id')->on('users')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->float('price');
+            $table->enum("status",["accepted","rejected"])->default(null);
+            $table->boolean("autoAkcept")->default(true);
+            $table->integer('nb_blace');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
--- Evento/routes/web.php
+++ Evento/routes/web.php
@@ -27,5 +27,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::get('/auth', function () {
+    return view('auth.auth');})->name('auth');
 
 require __DIR__.'/auth.php';
